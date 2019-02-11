0c6e44449cb1e651f38391e3b8190667e915ce56
wuwei <wudawei112345>
2019-02-11 18:16:21 +0800

    初始提交

---
+++ src/main/java/com/ht/eurekaserver/EurekaserverApplication.java
@@ -0,0 +1,14 @@
+package com.ht.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaserverApplication.class, args);
+	}
+
+}
+
